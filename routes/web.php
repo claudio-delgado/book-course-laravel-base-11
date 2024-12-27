778c22c72ecbbdfe7bebec335207a13a80adbc9f
cdelgado <=>
2024-12-27 11:35:56 -0300

    Laravel Breeze y adaptación pantallas.

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,26 @@
 <?php
-
-use App\Http\Controllers\Dashboard\PostController;
-use App\Http\Controllers\Dashboard\CategoryController;
-use App\Http\Controllers\PrimerControlador;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-})->name('welcome');
+});
 
-Route::get('test', [PrimerControlador::class, 'index']);
-Route::get('otro/{post?}/{otro?}', [PrimerControlador::class, 'otro']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 Route::group(['prefix' => "dashboard"], function(){
     Route::resources([
-        'post' => PostController::class,
-        'category' => CategoryController::class
+        'post' => App\Http\Controllers\Dashboard\PostController::class,
+        'category' => App\Http\Controllers\Dashboard\CategoryController::class
     ]);
 });
+
+require __DIR__.'/auth.php';
